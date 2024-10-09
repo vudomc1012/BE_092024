91d8545bb89a4fa397b90c0b71ce50c74f09955c
Hecker <you@example.com>
2024-10-09 12:34:30 +0700

    Buoi2 : Bai 6

---
+++ Solution_BE_092024/Solution_BE_092024/Baitap/Buoi2/Bai6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_BE_092024.Baitap.Buoi2
+{
+	public class Bai6
+	{
+		public void KiemTraChanLe()
+		{
+			var nhapSo = new Common.Validate();
+			int so;
+			so = (int)nhapSo.NhapSo("Nhập vào số nguyên ");
+			if (so % 2 == 0)
+			{
+				Console.WriteLine($"{so} là số chẵn.");
+			}
+			else
+			{
+				Console.WriteLine($"{so} là số lẻ.");
+			}
+		}
+	}
+}
