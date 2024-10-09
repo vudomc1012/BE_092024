8537aa918a60396bf34d7e0fd0ed6fc334bdf824
Hecker <you@example.com>
2024-10-09 12:41:00 +0700

    so nguyen to

---
+++ Solution_BE_092024/Solution_BE_092024/Baitap/SoNguyenTo/SoNguyenTo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_BE_092024.Baitap.SoNguyenTo
+{
+	public class SoNguyenTo
+	{
+		public bool LaSoNguyenTo(int so)
+		{
+			if (so < 2)return false;
+			
+			{
+				for (int i = 2; i <= Math.Sqrt(so); i++)
+				{
+					if (so % i == 0) return false;
+				}
+				return true;
+			}
+		}
+
+	}
+}
