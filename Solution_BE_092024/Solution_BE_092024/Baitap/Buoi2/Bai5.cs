22aee887ec8ff8e9c8af31da9f338afcd435d638
Hecker <you@example.com>
2024-10-09 12:34:12 +0700

    bai tap buoi 2: Bai 5

---
+++ Solution_BE_092024/Solution_BE_092024/Baitap/Buoi2/Bai5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_BE_092024.Baitap.Buoi2
+{
+	public class Bai5
+	{
+		//		Bài 5: Viết
+		//chương trình C# liệt kê tất cả các số nguyên tố nhỏ hơn n.
+		public void SoNguyenToNhoHonN()
+		{
+			var LaSoNguyenTo = new SoNguyenTo.SoNguyenTo();
+			var NhapSo = new Common.Validate();
+			int n = (int)NhapSo.NhapSo("Nhập vào một số nguyên dương: ");
+			for (int i = 2; i < n; i++)
+			{
+				if (LaSoNguyenTo.LaSoNguyenTo(i))
+				{
+					Console.WriteLine("\nNhững số nguyên tố nhỏ hơn n: {0}\n",i);
+					
+				}
+			}
+		}
+	}
+}
--- Solution_BE_092024/Solution_BE_092024/Program.cs
+++ Solution_BE_092024/Solution_BE_092024/Program.cs
@@ -21,6 +21,8 @@ namespace Solution_BE_092024
 				Console.WriteLine("2. Giải phương trình bậc 1 và bậc 2");
 				Console.WriteLine("3. Chuyển đổi độ C thành độ K và độ F");
 				Console.WriteLine("4. Tính giai thừa của một số");
+				Console.WriteLine("5. Kiểm tra số nguyên tố nhỏ hơn N");
+				Console.WriteLine("6. Kiểm tra chẵn lẻ ");
 				Console.Write("\nNhập lựa chọn của bạn: ");
 				var NhapSo = new Validate();
 				int luaChon = (int)NhapSo.NhapSo("");
@@ -40,6 +42,12 @@ namespace Solution_BE_092024
 						break;
 					case 4:
 						Bai4();
+						break;
+					case 5:
+						Bai5();
+						break;
+					case 6:
+						Bai6();
 						break;
 					default:
 						Console.WriteLine("Lựa chọn không hợp lệ. Vui lòng thử lại.");
@@ -83,5 +91,23 @@ namespace Solution_BE_092024
 
 		}
 		#endregion
+
+		#region Bài 5: Liệt kê tất cả các số nguyên tố nhỏ hơn n
+		static void Bai5()
+		{
+			var bai5 = new Solution_BE_092024.Baitap.Buoi2.Bai5();
+			bai5.SoNguyenToNhoHonN();
+		}
+		#endregion
+
+		#region Bài 6: Kiểm tra số chẵn hay lẻ
+		static void Bai6()
+		{
+			var bai6 = new Solution_BE_092024.Baitap.Buoi2.Bai6();
+			bai6.KiemTraChanLe();
+		}
+		#endregion
+
+		
 	}
 }
